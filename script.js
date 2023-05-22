0eda77f011f0f55c00f1557109bf8737e3315fbb
fernandavictoria <fernandamayfield@gmail.com>
2023-05-22 13:35:54 -0300

    primeira atualização

---
+++ script.js
@@ -0,0 +1,15 @@
+const listElement = document.querySelector("#app ul")
+const inputElement = document.querySelector("#app input")
+const buttonElement = document.querySelector("#app button")
+
+const tarefas = ["Estudar para a prova", "Dormir cedo"]
+
+for (const iterator of tarefas) {
+    const tarefaElement = document.createElement("li")
+    const tarefaText = document.createTextNode(iterator)
+
+    tarefaElement.appendChild(tarefaText)
+    listElement.appendChild(tarefaElement)
+    //console.log(iterator)
+}
+//console.log(listElement,inputElement,buttonElement)
